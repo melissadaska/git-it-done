45427eeec6df2dbebb7866b6455f779e2c80b6ae
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-04-19 16:54:49 -0500

    add form and event listener

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,4 +10,22 @@ var getUserRepos = function(user) {
     });
 };
 
-getUserRepos();
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    // check if there's a value in that username, if yes then pass that data to getUserRepos, then to clear the form we clear out the <input> element's value
+    if(username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    // else alert that they need to enter a username with a value
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
