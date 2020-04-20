a09e31176f01da66e87633a4cf1f82b635091071
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-04-20 13:01:34 -0500

    add error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,11 +3,21 @@ var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoName = function() {
+    // grab repo name from url query string
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
-}
+
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+
+        getRepoIssues(repoName); 
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var displayWarning = function(repo) {
     // add text to warning container
@@ -38,7 +48,8 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
